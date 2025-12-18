14118b7ce75a69d4d41ecb1347ed51b8bb5407f6
Ota Fejfar <ota.fejfar@gmail.com>
2025-12-18 15:31:13 +0100

    chore: Enabling the Sonshi configuration parameters
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -16,143 +16,140 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-//#include "Battery.h"
-//#include "Ble_manager.h"
+#include "Battery.h"
+#include "Ble_manager.h"
 #include "configuration.h"
 #include "Config_manager.h"
-//#include "DynamicMacrosDygma.h"
-//#include "EEPROMKeymapDygma.h"
-//#include "FirmwareVersion.h"
-//#include "KeyRoleManager.h"
-//#include "LEDLayers.h"
-//#include "LEDManager.h"
-//#include "LEDPaletteRGBW.h"
-//#include "MouseKeysDygma.h"
-//#include "QukeysDygma.h"
-//#include "Radio_manager.h"
-//#include "SuperkeysHandler.h"
+#include "DynamicMacrosDygma.h"
+#include "EEPROMKeymapDygma.h"
+#include "FirmwareVersion.h"
+#include "KeyRoleManager.h"
+#include "LEDLayers.h"
+#include "LEDManager.h"
+#include "LEDPaletteRGBW.h"
+#include "MouseKeysDygma.h"
+#include "QukeysDygma.h"
+#include "SuperkeysHandler.h"
 
 typedef struct PACK __attribute__((aligned(4)))
 {
-//    LEDManager::ledmanager_conf_t ledmanager;
-//    kaleidoscope::plugin::Qukeys::Qukeys_config_t Qukeys;
-//    kaleidoscope::plugin::MouseKeys_::MouseKeys_config_t MouseKeys;
-//    FirmwareVersion::device_spec_t device_spec;
-//    Battery::battery_conf_t battery;
-//    BleManager::connections_config_t ble_connections;
-//    RadioManager::rf_config_t rf;
-//    kaleidoscope::plugin::EEPROMKeymap::keymap_config_t keymap;
-//    LEDPaletteRGBW::palette_config_t palette;
-//    LEDLayers::colormap_config_t colormap;
-//    kaleidoscope::plugin::KeyRoleManager::keyrole_config_t keyrole;
-//    kaleidoscope::plugin::SuperkeysHandler::superkey_config_t superkey;
-//    kaleidoscope::plugin::DynamicMacros::macros_config_t macros;
+    FirmwareVersion::device_spec_t device_spec;
+
+    kaleidoscope::plugin::EEPROMKeymap::keymap_config_t keymap;
+    kaleidoscope::plugin::MouseKeys_::MouseKeys_config_t MouseKeys;
+    kaleidoscope::plugin::KeyRoleManager::keyrole_config_t keyrole;
+    kaleidoscope::plugin::Qukeys::Qukeys_config_t Qukeys;
+    kaleidoscope::plugin::SuperkeysHandler::superkey_config_t superkey;
+    kaleidoscope::plugin::DynamicMacros::macros_config_t macros;
+
+    LEDManager::ledmanager_conf_t ledmanager;
+    LEDPaletteRGBW::palette_config_t palette;
+    LEDLayers::colormap_config_t colormap;
+
+    Battery::battery_conf_t battery;
+
+    BleManager::connections_config_t ble_connections;
+
 } config_cache_t;
 
 static config_cache_t config_cache;
 
 static result_t _cfg_item_request_cb( ConfigManager::cfg_item_type_t item_type, const void ** pp_item )
 {
-//    switch( item_type )
-//    {
-//        case ConfigManager::CFG_ITEM_TYPE_DEVICE_SPEC:
-//
-//            *pp_item = &config_cache.device_spec;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_LEDS_LEDMANAGER:
-//
-//            *pp_item = &config_cache.ledmanager;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_LEDS_PALETTE:
-//
-//            *pp_item = &config_cache.palette;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_LEDS_COLORMAP:
-//
-//            *pp_item = &config_cache.colormap;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_BATTERY:
-//
-//            *pp_item = &config_cache.battery;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_BLE_CONNECTIONS:
-//
-//            *pp_item = &config_cache.ble_connections;
-//
-//            break;
-//
-//        case ConfigManager::CFG_ITEM_TYPE_RF:
-//
-//            *pp_item = &config_cache.rf;
-//
-//            break;
-//
-//        default:
-//
-//            ASSERT_DYGMA( false, "Invalid KBDMEM item requested" );
-//
-//            return RESULT_ERR;
-//    }
+    switch( item_type )
+    {
+        case ConfigManager::CFG_ITEM_TYPE_DEVICE_SPEC:
+
+            *pp_item = &config_cache.device_spec;
+
+            break;
+
+        case ConfigManager::CFG_ITEM_TYPE_LEDS_LEDMANAGER:
+
+            *pp_item = &config_cache.ledmanager;
+
+            break;
+
+        case ConfigManager::CFG_ITEM_TYPE_LEDS_PALETTE:
+
+            *pp_item = &config_cache.palette;
+
+            break;
+
+        case ConfigManager::CFG_ITEM_TYPE_LEDS_COLORMAP:
+
+            *pp_item = &config_cache.colormap;
+
+            break;
+
+        case ConfigManager::CFG_ITEM_TYPE_BATTERY:
+
+            *pp_item = &config_cache.battery;
+
+            break;
+
+        case ConfigManager::CFG_ITEM_TYPE_BLE_CONNECTIONS:
+
+            *pp_item = &config_cache.ble_connections;
+
+            break;
+
+        default:
+
+            ASSERT_DYGMA( false, "Invalid KBDMEM item requested" );
+
+            return RESULT_ERR;
+    }
 
     return RESULT_OK;
 }
 
 static result_t _cfg_item_request_kbdmem_cb( kbdmem_item_type_t item_type, const void ** pp_item )
 {
-//    switch( item_type )
-//    {
-//        case KBDMEM_ITEM_TYPE_QUKEYS:
-//
-//            *pp_item = &config_cache.Qukeys;
-//
-//            break;
-//
-//        case KBDMEM_ITEM_TYPE_MOUSEKEYS:
-//
-//            *pp_item = &config_cache.MouseKeys;
-//
-//            break;
-//
-//        case KBDMEM_ITEM_TYPE_KEYMAP:
-//
-//            *pp_item = &config_cache.keymap;
-//
-//            break;
-//
-//        case KBDMEM_ITEM_TYPE_KEYROLE:
-//
-//            *pp_item = &config_cache.keyrole;
-//
-//            break;
-//
-//        case KBDMEM_ITEM_TYPE_SUPERKEY:
-//
-//            *pp_item = &config_cache.superkey;
-//
-//            break;
-//
-//        case KBDMEM_ITEM_TYPE_MACROS:
-//
-//            *pp_item = &config_cache.macros;
-//
-//            break;
-//
-//        default:
-//
-//            ASSERT_DYGMA( false, "Invalid KBDMEM item requested" );
-//
-//            return RESULT_ERR;
-//    }
+    switch( item_type )
+    {
+        case KBDMEM_ITEM_TYPE_KEYMAP:
+
+            *pp_item = &config_cache.keymap;
+
+            break;
+
+        case KBDMEM_ITEM_TYPE_MOUSEKEYS:
+
+            *pp_item = &config_cache.MouseKeys;
+
+            break;
+
+        case KBDMEM_ITEM_TYPE_KEYROLE:
+
+            *pp_item = &config_cache.keyrole;
+
+            break;
+
+        case KBDMEM_ITEM_TYPE_QUKEYS:
+
+            *pp_item = &config_cache.Qukeys;
+
+            break;
+
+        case KBDMEM_ITEM_TYPE_SUPERKEY:
+
+            *pp_item = &config_cache.superkey;
+
+            break;
+
+        case KBDMEM_ITEM_TYPE_MACROS:
+
+            *pp_item = &config_cache.macros;
+
+            break;
+
+        default:
+
+            ASSERT_DYGMA( false, "Invalid KBDMEM item requested" );
+
+            return RESULT_ERR;
+    }
 
     return RESULT_OK;
 }
